c4b172b2d73ad57f1f6447cda0b8e02f2913e8a7
GvozdenMiskovic <ironman.requests@gmail.com>
2016-10-20 10:49:24 +0200

    time data normalization added

--- calculateStatistics.js
+++ calculateStatistics.js
@@ -11,24 +11,42 @@ var totalLosses = 4;
 //end static data
 
 
-var averageGameTime = function averageGameTime() { //output average time of a game
+function averageGameTime() { //output average time of a game
     return gameTime/totalGames;
-};
+}
 
 console.log("Average Game Time"); //for debugging
 console.debug(averageGameTime()); //for debugging
 
 
-var averageTimeWhenWinning = function averageTimeWhenWinning() { //output average time of game when winning
+function averageTimeWhenWinning() { //output average time of game when winning
     return timeWhenWinning/totalWins;
-};
+}
 
 console.log("Average Time When Winning"); //for debugging
 console.debug(averageTimeWhenWinning()); //for debugging
 
-var averageTimeWhenLosing = function averageTimeWhenLosing() { //output average time of game when losing
+function averageTimeWhenLosing() { //output average time of game when losing
     return timeWhenLosing/totalLosses;
-};
+}
 
 console.log("Average time When Losing"); //for debugging
 console.debug(averageTimeWhenLosing()); //for debugging
+
+function normalize(time) { //normalize data to minute:second format
+    var timeMinutes = time / 60;
+    var minutes = parseInt(timeMinutes, 10);
+    var seconds = parseInt((timeMinutes - minutes)*10,10);
+    return { //returns object with minutes and seconds as properties
+        "minutes": minutes,
+        "seconds": seconds
+    }
+}
+
+var normalizedAverageGameTime = normalize(averageGameTime()); //set to normalized avg game time
+var normalizedAverageTimeWhenWinning = normalize(averageTimeWhenWinning()); //set to normalized avg game time when win
+var normalizedAverageTimeWhenLosing = normalize(averageTimeWhenLosing()); //set to normalized avg game time when loss
+
+console.debug(normalizedAverageGameTime);
+console.debug(normalizedAverageTimeWhenWinning);
+console.debug(normalizedAverageTimeWhenLosing);
